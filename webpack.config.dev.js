e869b309784743d6b3f618f37c16c8e67745da56
Daimeng Chen <daimengchen@gmail.com>
2021-11-30 21:22:08 -0500

    config updates

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -6,21 +6,22 @@ module.exports = merge(common, {
   mode: 'development',
   target: 'web',
   devServer: {
-    contentBase: 'src',
-    watchContentBase: true,
+    // contentBase: 'src',
+    // watchContentBase: true,
+    watchFiles: 'src/*',
     hot: true,
     open: false,
     port: process.env.PORT || 9001,
     host: process.env.HOST || 'localhost',
-    stats: {
-      // Config for minimal console.log mess.
-      colors: true,
-      version: false,
-      hash: false,
-      timings: false,
-      chunks: false,
-      chunkModules: false
-    }
+    // stats: {
+    //   // Config for minimal console.log mess.
+    //   colors: true,
+    //   version: false,
+    //   hash: false,
+    //   timings: false,
+    //   chunks: false,
+    //   chunkModules: false
+    // }
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
 });
